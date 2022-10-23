bc95515f4adb6cbb6afe3c2a80aa6cbe84a1b1b3
unknown <tusharmittal0109@gmail.com>
2022-10-24 00:44:35 +0530

    Added comments to studentDetails file

--- OOPS 1/StudentDetails_Class.cpp
+++ OOPS 1/StudentDetails_Class.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+#include <iostream> // importing library
 
 using namespace std;
 
+// Creating a class to store input from user of student details and display
 class student{
     char name [20];
     int roll;
@@ -9,7 +10,7 @@ class student{
     char fathername[20];
     char mothername[20];
     public:
-        void getData(){
+        void getData(){ // Function to get data from user
             cout << "Enter name: " << endl;
             cin >> name;
             cout << "Enter roll no.: " << endl;
@@ -21,7 +22,7 @@ class student{
             cout << "Enter mother name: " << endl;
             cin >> mothername;
         }
-        void displayData(){
+        void displayData(){ // Function to display user entered data
             cout << "Name : " << name << endl;
             cout << "Roll Number : " << roll << endl;
             cout << "Contact : " << contact << endl;
@@ -30,15 +31,15 @@ class student{
         }
 };
 
-int main(){
+int main(){ // main function 
     int i;
-    student s1,s2,s3,s4,s5;
-    s1.getData();
+    student s1,s2,s3,s4,s5; // creating objects of class student to store and display data
+    s1.getData(); // using getData function to store or get data from user
     s2.getData();
     s3.getData();
     s4.getData();
     s5.getData();
-    s1.displayData();
+    s1.displayData(); // using displayData function to display student data
     s2.displayData();
     s3.displayData();
     s4.displayData();
